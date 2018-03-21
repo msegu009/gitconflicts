247f852d6c2f39f928819d898291d3eb190b3722
Michael Segura <msegu009@outlook.com>
2018-03-21 10:33:39 -0400

    mike second change

--- game.rb
+++ game.rb
@@ -3,3 +3,7 @@ require './engine.rb'
 puts "Welcome to Wyncodium"
 puts "Enter your Name: "
 user_name= gets.chomp
+if user_name = "mike"
+	puts "oh, it's you..."
+else
+	puts 'Welcome, #{user_name} !'
